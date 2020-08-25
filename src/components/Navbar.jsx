a51f3e5c9fbaf8b05e2c08d2ff5708b04e26bb18
aniketbadole <aniket.b@outlook.com>
2020-08-25 23:38:09 +0530

    Fixing navigation errors

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "../assets/logo.png";
-import { Link, animateScroll as scroll } from "react-scroll";
+import { Link } from "react-scroll";
 import MainPage from "./MainPage";
 const Navbar = () => {
   return (
